f34d2a6efa9091388ac721e04cfa3cfb79048656
shrfrn <shrfrn@gmail.com>
2023-06-05 22:59:28 +0300

    Simplified: no pointer-events: none. Less js.

--- js/flex-and-position.js
+++ js/flex-and-position.js
@@ -1,30 +1,14 @@
 function openModal() {
-    const elBody = document.querySelector('body')
-    const elMain = document.querySelector('main')
     const elBackdrop = document.querySelector('.backdrop')
     const elModal = document.querySelector('.modal')
     
-    elBackdrop.style.opacity = 1
-    elBackdrop.style.pointerEvents = 'auto'
-    
-    elModal.style.opacity = 1
-    elModal.style.pointerEvents = 'auto'
-    
-    elMain.style.pointerEvents = 'none'
-    elBody.style.overflow = 'hidden'
+    elModal.classList.add('opaque')
+    elBackdrop.classList.add('opaque')
 }
 function closeModal() {
-    const elBody = document.querySelector('body')
-    const elMain = document.querySelector('main')
     const elBackdrop = document.querySelector('.backdrop')
     const elModal = document.querySelector('.modal')
     
-    elBackdrop.style.opacity = 0
-    elBackdrop.style.pointerEvents = 'none'
-    
-    elModal.style.opacity = 0
-    elModal.style.pointerEvents = 'none'
-    
-    elMain.style.pointerEvents = 'auto'
-    elBody.style.overflow = 'scroll'
+    elModal.classList.remove('opaque')
+    elBackdrop.classList.remove('opaque')
 }
