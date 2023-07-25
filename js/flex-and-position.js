f1017ee26eef7dab6aaf8e6ea2e1235529c12eb1
shrfrn <shrfrn@gmail.com>
2023-07-25 16:43:08 +0300

    Added new modal design

--- js/flex-and-position.js
+++ js/flex-and-position.js
@@ -1,14 +1,29 @@
+'use strict'
+var gCurrArea = 'area3'
+
 function openModal() {
     const elBackdrop = document.querySelector('.backdrop')
-    const elModal = document.querySelector('.modal')
-    
-    elModal.classList.add('opaque')
     elBackdrop.classList.add('opaque')
 }
-function closeModal() {
+
+function closeModal(ev, el) {
     const elBackdrop = document.querySelector('.backdrop')
-    const elModal = document.querySelector('.modal')
-    
-    elModal.classList.remove('opaque')
     elBackdrop.classList.remove('opaque')
+}
+
+function moveAround(elCircle) {
+    const gridAreas = ['area1', 'area2', 'area3', 'area4', 'area5']
+    var idx = getRandomInt(0, gridAreas.length)
+
+    while(gridAreas[idx] === gCurrArea){
+        idx = getRandomInt(0, gridAreas.length)
+    }
+    gCurrArea = gridAreas[idx]
+    elCircle.style.gridArea = gridAreas[idx]
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
 }
